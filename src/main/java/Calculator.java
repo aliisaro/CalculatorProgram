57eeaf5cee5d8f3da1038b8a600b05f0d3ed8f4c
Aliisa <aliisarokala@protonmail.com>
2024-09-27 09:58:43 +0300

    update

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Done docker");
+    }
 }
+
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class CalculatorTest {
+import org.junit.jupiter.api.Test;
+import static junit.framework.Assert.assertEquals;
+public class CalculatorTest {
+
+    Calculator calculator= new Calculator();
+
+    @Test
+    public void multiplyTest(){
+        assertEquals(10, calculator.multiply(5,2));
+    }
+
+    @Test
+    public void subtractTest(){
+        assertEquals(3, calculator.subtract(5,2));
+    }
+
+    @Test
+    public void divideTest(){
+        assertEquals(3, calculator.divide(6,2));
+    }
 }
